1a522c7bc944e0ef477e38208b65973310a9b65a
NanaCs08 <82248322+NanaCs08@users.noreply.github.com>
2024-11-06 19:56:16 -0600

    commit 2

--- components/header.vue
+++ components/header.vue
@@ -1,15 +1,3 @@
-<script setup>
-import { onMounted } from 'vue';
-
-onMounted(() => {
-  if (typeof window !== 'undefined' && window.Snipcart) {
-    window.Snipcart.init({
-      publicApiKey: 'ZDU3NDg5NzEtZGNhZS00NjgxLTkxYzAtNjE5OTA4ZTk0ZWM5NjM4NjY0NTc4NDc3NTk4Mjc5',
-      currency: 'usd' // Cambia a la moneda que estés usando
-    });
-  }
-});
-</script>
 <template>
     <div class="row"  style="margin-top: 15px">
       <NuxtLink class="two columns button button-primary" to="/">
@@ -19,7 +7,7 @@ onMounted(() => {
       <NuxtLink class="two columns button button-primary" to="/fabricantes">
         Fabricantes</NuxtLink>
       <NuxtLink class="three columns button button-primary" to="/aerolineas">Aerolineas</NuxtLink>
-          <button class="snipcart-checkout three columns button"><span class="snipcart-items-count">0</span> books in the 🛒 </button>
+          <button class="snipcart-checkout three columns button"><span class="snipcart-items-count">0</span> Tiquets in the 🛒 </button>
     </div>
   </template>
 
